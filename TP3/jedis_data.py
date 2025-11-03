ffd5abeae839eb84c680116a6fb16081f8c05c59
paula.rodriguez992 <mariapaulaursula@gmail.com>
2025-11-03 19:10:08 -0300

    ej16 agregado

---
+++ TP3/jedis_data.py
@@ -0,0 +1,64 @@
+# jedi_data.py
+
+jedis = [
+    {
+        "nombre": "Luke Skywalker",
+        "especie": "Humana",
+        "maestros": ["Obi-Wan Kenobi", "Yoda"],
+        "colores_sable": ["verde", "azul"]
+    },
+    {
+        "nombre": "Yoda",
+        "especie": "Desconocida",
+        "maestros": [],
+        "colores_sable": ["verde"]
+    },
+    {
+        "nombre": "Ahsoka Tano",
+        "especie": "Togruta",
+        "maestros": ["Anakin Skywalker"],
+        "colores_sable": ["blanco", "verde"]
+    },
+    {
+        "nombre": "Kit Fisto",
+        "especie": "Nautolano",
+        "maestros": ["Yoda"],
+        "colores_sable": ["verde"]
+    },
+    {
+        "nombre": "Mace Windu",
+        "especie": "Humana",
+        "maestros": ["Yoda"],
+        "colores_sable": ["violeta"]
+    },
+    {
+        "nombre": "Qui-Gon Jinn",
+        "especie": "Humana",
+        "maestros": ["Conde Dooku"],
+        "colores_sable": ["verde"]
+    },
+    {
+        "nombre": "Obi-Wan Kenobi",
+        "especie": "Humana",
+        "maestros": ["Qui-Gon Jinn", "Yoda"],
+        "colores_sable": ["azul"]
+    },
+    {
+        "nombre": "Aayla Secura",
+        "especie": "Twi'lek",
+        "maestros": ["Quinlan Vos"],
+        "colores_sable": ["azul"]
+    },
+    {
+        "nombre": "Plo Koon",
+        "especie": "Kel Dor",
+        "maestros": ["Tyvokka"],
+        "colores_sable": ["azul", "amarillo"] 
+    },
+    {
+        "nombre": "Depa Billaba",
+        "especie": "Humana",
+        "maestros": ["Mace Windu"],
+        "colores_sable": ["verde"]
+    }
+]
